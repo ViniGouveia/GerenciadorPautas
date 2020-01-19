3b80b572dae9849317f5f276baeb5b1861a53865
Vinicius Gouveia <vini.gouveia11@gmail.com>
2020-01-18 22:38:33 -0300

    Login atualizado e funcionando

--- app/src/main/java/com/vinigouveia/gerenciadorpautas/activities/LoginActivity.java
+++ app/src/main/java/com/vinigouveia/gerenciadorpautas/activities/LoginActivity.java
@@ -7,18 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.vinigouveia.gerenciadorpautas.R;
+import com.vinigouveia.gerenciadorpautas.Room.DBData.MyDatabase;
+import com.vinigouveia.gerenciadorpautas.Room.DBEntities.UserEntity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Instancia do objeto utilizado para mapear os elementos gráficos da Activity
     private LoginViewHolder mLoginViewHolder = new LoginViewHolder();
+    private Intent intentToRecoverPassword;
+    private Intent intentToCreateAccount;
+    private Intent intentToAgenda;
+    private MyDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        db = MyDatabase.getAppDatabase(getApplicationContext());
+        intentToRecoverPassword = new Intent(getApplicationContext(), RecoverPasswordActivity.class);
+        intentToCreateAccount = new Intent(getApplicationContext(), CreateAccountActivity.class);
+        intentToAgenda = new Intent(getApplicationContext(), AgendaActivity.class);
 
         //Mapeamento dos elementos gráficos
         this.mLoginViewHolder.textEmail = findViewById(R.id.text_email);
@@ -36,9 +48,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        Intent intentToRecoverPassword = new Intent(getApplicationContext(), RecoverPasswordActivity.class);
-        Intent intentToCreateAccount = new Intent(getApplicationContext(), CreateAccountActivity.class);
-        Intent intentToAgenda = new Intent(getApplicationContext(), AgendaActivity.class);
         if (v.getId() == R.id.button_recover_password) {
             startActivity(intentToRecoverPassword);
         }
@@ -46,8 +55,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(intentToCreateAccount);
         }
         if (v.getId() == R.id.button_login) {
-            //Falta fazer a validação dos dados no banco de dados
+            String email = mLoginViewHolder.textEmail.getText().toString();
+            String password = mLoginViewHolder.textPassword.getText().toString();
+            if (email.equals("") || password.equals("")) {
+                Toast.makeText(getApplicationContext(), "Preencha o email e a senha", Toast.LENGTH_LONG).show();
+            } else {
+                UserEntity user = db.userDao().getUser(email);
+                verifyUser(user, password);
+            }
+        }
+    }
+
+    public void verifyUser(UserEntity user, String password) {
+        if (user == null) {
+            Toast.makeText(getApplicationContext(), "Usuário não encontrado", Toast.LENGTH_LONG).show();
+        } else if (user.getUserPassword().equals(password)) {
             startActivity(intentToAgenda);
+        } else {
+            Toast.makeText(getApplicationContext(), "Senha incorreta", Toast.LENGTH_LONG).show();
         }
     }
 
